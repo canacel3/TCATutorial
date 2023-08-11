fdb5d1d11b0ec37b1bc212b43a941340266a18c8
canacel3 <nishidakaede3@gmail.com>
2023-08-11 15:33:52 +0900

    Testing your feature

--- Counter/Counter/CounterFeature.swift
+++ Counter/Counter/CounterFeature.swift
@@ -27,6 +27,9 @@ struct CounterFeature: ReducerProtocol {
 
     enum CancelID { case timer }
 
+    @Dependency(\.continuousClock) var clock
+    @Dependency(\.numberFact) var numberFact
+
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
         case .decrementButtonTapped:
@@ -44,10 +47,7 @@ struct CounterFeature: ReducerProtocol {
             state.isLoading = true
 
             return .run { [count = state.count] send in
-                let (data, _) = try await URLSession.shared
-                    .data(from: URL(string: "http://numbersapi.com/\(count)")!)
-                let fact = String(decoding: data, as: UTF8.self)
-                await send(.factResponse(fact))
+                try await send(.factResponse(self.numberFact.fetch(count)))
             }
 
         case let .factResponse(fact):
@@ -59,8 +59,7 @@ struct CounterFeature: ReducerProtocol {
             state.isTimerRunning.toggle()
             if state.isTimerRunning {
                 return .run { send in
-                    while true {
-                        try await Task.sleep(for: .seconds(1))
+                    for await _ in self.clock.timer(interval: .seconds(1)) {
                         await send(.timerTick)
                     }
                 }
@@ -76,3 +75,5 @@ struct CounterFeature: ReducerProtocol {
         }
     }
 }
+
+extension CounterFeature.Action: Equatable {}
